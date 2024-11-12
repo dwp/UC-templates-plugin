a2aa1ca630bd54a6d13fc042de33a349ba6a315b
Simone Duca <simone.duca@engineering.digital.dwp.gov.uk>
2024-11-12 13:46:33 +0000

    chore: adds layout inheritance and removes serviceName

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 module.exports = (router) => {
-  router.get("uc-agent-template", (req, res) => {
-    const serviceName = "UC Plugin";
-    res.render("/uc-agent-template.njk", { serviceName });
+  router.get("/uc-agent-template", (req, res) => {
+    res.render("/uc-agent-template.njk");
   });
 };
