c96c716a8241c60c00219617ae815b67d48a09c1
Simone Duca <simone.duca@engineering.digital.dwp.gov.uk>
2024-11-18 11:25:37 +0000

    chore: include layouts in templates

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 module.exports = (router) => {
-  router.get("/uc-agent-template", (req, res) => {
-    res.render("/uc-agent-template.njk");
-  });
+  // router.get("/uc-agent-template", (req, res) => {
+  //   res.render("/uc-agent-template.njk");
+  // });
 };
